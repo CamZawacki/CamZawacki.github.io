05b32ab40b4d5f676ec9d746a013f44d680db061
CamZawacki <zawackicameron@gmail.com>
2016-05-17 16:44:21 -0400

    image

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,4 @@
-console.log('This would be the main JS file.');
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    document.getElementsByClassName("topnav")[0].classList.toggle("responsive");
+}
