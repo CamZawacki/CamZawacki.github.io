10afc059bbe841d7ae95f819c0976845088f9849
CamZawacki <zawackicameron@gmail.com>
2016-05-17 19:14:25 -0400

    nav bar

--- javascripts/script.js
+++ javascripts/script.js
@@ -2,5 +2,5 @@ document.getElementById("topnav").innerHTML =
 "<ul id='topnav'>" +
 	"<li><a href=''>Home</a></li>" + 
   	"<li><a href='../projects/project1'>Project 1</a></li>" + 
-  	"<li><a href='../projects/project1'>Project 2</a></li>" + 
+  	"<li><a href='../projects/project2'>Project 2</a></li>" + 
 "</ul>";
