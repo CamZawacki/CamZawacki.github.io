a5e15e3ab483d14aaf9e040d83ff94ce8dd8943c
CamZawacki <zawackicameron@gmail.com>
2016-05-17 19:46:54 -0400

    Project files

---
+++ javascripts/navbar.js
@@ -0,0 +1,6 @@
+document.getElementById("topnav").innerHTML =
+"<ul id='topnav'>" +
+	"<li><a href='../'>Home</a></li>" + 
+  	"<li><a href='../projects/Hemoglobetrotter'>Hemoglobetrotter</a></li>" + 
+  	"<li><a href='../projects/Theg-Ame'>Theg-Ame</a></li>" + 
+"</ul>";
