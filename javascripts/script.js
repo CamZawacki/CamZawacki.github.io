aa2c8ae3d112c20068125c9887a43d8ab0da78d0
CamZawacki <zawackicameron@gmail.com>
2016-05-17 19:01:14 -0400

    nav bar

--- javascripts/script.js
+++ javascripts/script.js
@@ -5,5 +5,14 @@ document.getElementById("nav01").innerHTML =
 "</ul>";
 
 function myFunction() {
-    document.getElementsByClassName("topnav")[0].classList.toggle("responsive");
+    document.getElementsByClassName("topnav")[0].innerHTML =
+"<ul class='topnav'>" + 
+  "<li><a href='#home'>Home</a></li>" + 
+  "<li><a href='#news'>News</a></li>" + 
+  "<li><a href='#contact'>Contact</a></li>" + 
+  "<li><a href='#about'>About</a></li>" + 
+  "<li class='icon'>" + 
+    "<a href='javascript:void(0);' onclick='myFunction()'>&#9776;</a>" +
+  "</li>" + 
+"</ul>";
 }
