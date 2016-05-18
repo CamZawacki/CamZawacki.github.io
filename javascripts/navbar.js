ad03d377b497850145b8593a51853f809591fae7
CamZawacki <zawackicameron@gmail.com>
2016-05-17 22:27:11 -0400

    Project files

--- javascripts/navbar.js
+++ javascripts/navbar.js
@@ -3,7 +3,7 @@ document.getElementById("topnav").innerHTML =
 	"<li><a href='../'>Home</a></li>" + 
   	"<li><a href='../projects/Hemoglobetrotter'>Hemoglobetrotter</a></li>" + 
   	"<li><a href='../projects/Theg-Ame'>Theg-Ame</a></li>" + 
-  	"<li><a href='../projects/Photography'>TPhotography</a></li>" + 
+  	"<li><a href='../projects/Photography'>Photography</a></li>" + 
   	"<li><a href='../about'>About Me</a></li>" +
   	"<li><a href='../resume'>Resume</a></li>" +
 "</ul>";
