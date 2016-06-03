2a8b9a260fcd5870c5fe735c2c3b2f5ac770ce13
CamZawacki <zawackicameron@gmail.com>
2016-06-03 00:34:35 -0400

    stuff

--- javascripts/navbar.js
+++ javascripts/navbar.js
@@ -2,7 +2,6 @@ document.getElementById("topnav").innerHTML =
 "<ul id='topnav'>" +
 	"<li><a href='../'>Home</a></li>" + 
   	"<li><a href='../projects/Hemoglobetrotter'>Hemoglobetrotter</a></li>" + 
-  	"<li><a href='../projects/Theg-Ame'>Theg-Ame</a></li>" + 
   	"<li><a href='../projects/Photography'>Photography</a></li>" + 
   	"<li><a href='../resume/resume'>Resume</a></li>" +
 "</ul>";
