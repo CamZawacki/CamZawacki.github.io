1161dc477db880d4febdfa2d03a7b5e591ec360b
CamZawacki <zawacki.cameron@gmail.com>
2017-01-07 13:18:40 -0500

    navbar debugging

--- javascripts/navbar.js
+++ javascripts/navbar.js
@@ -4,5 +4,6 @@ document.getElementById("topnav").innerHTML =
 	"<li><a href='../projects/PCB_Etching'>PCB_Etching</a></li>" + 
   	"<li><a href='../projects/Hemoglobetrotter'>Hemoglobetrotter</a></li>" + 
   	"<li><a href='../projects/Photography'>Photography</a></li>" + 
+  	"<li><a href='../projects/Photography'>Photography</a></li>" + s
   	"<li><a href='../resume/resume'>Resume</a></li>" +
 "</ul>";
