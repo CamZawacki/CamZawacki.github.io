dbd027c3e78a47f93c3a6f52de36261e96f5a192
CamZawacki <zawacki.cameron@gmail.com>
2017-01-08 14:21:21 -0500

    Centering

--- javascripts/navbar.js
+++ javascripts/navbar.js
@@ -1,8 +1,8 @@
 document.getElementById("topnav").innerHTML =
 "<ul id='topnav'>" +
 	"<li><a href='../'>Home</a></li>" + 
+	"<li><a href='../projects/Hemoglobetrotter'>Hemoglobetrotter</a></li>" + 
 	"<li><a href='../projects/PCB_Etching'>PCB Etching</a></li>" + 
-  	"<li><a href='../projects/Hemoglobetrotter'>Hemoglobetrotter</a></li>" + 
   	"<li><a href='../projects/Small_Projects'>Misc. Projects</a></li>" + 
   	"<li><a href='../projects/Photography'>Photography</a></li>" + 
   	"<li><a href='../resume/resume'>Resume</a></li>" +
