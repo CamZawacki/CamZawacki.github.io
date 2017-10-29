f6989cb445b5e05f9674a52a0f75d9f06f566433
CamZawacki <zawacki.cameron@gmail.com>
2017-10-29 11:50:55 -0400

    max width ^

--- javascripts/navbar.js
+++ javascripts/navbar.js
@@ -1,7 +1,7 @@
 document.getElementById("topnav").innerHTML =
 "<ul id='topnav'>" +
 	"<li><a href='../'>Home</a></li>" + 
-	"<li><a href='../projects/Bandit'>Multi-Armed Bandits</a></li>" + 
+	"<li><a href='../projects/Bandit'>Multiarmed Bandits</a></li>" + 
 	"<li><a href='../projects/Hemoglobetrotter'>Hemoglobetrotter</a></li>" + 
 	"<li><a href='../projects/PCB_Etching'>PCB Etching</a></li>" + 
   	"<li><a href='../projects/Small_Projects'>Misc. Projects</a></li>" + 
